7dbb151c389028d5ab0191c5dfba0a11ade7c939
DewanH06 <115489028+DewanH06@users.noreply.github.com>
2022-12-02 11:29:32 -0500

    Add files via upload

---
+++ IproductRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using Testing.Models;
+namespace Testing
+{
+    public interface IproductRepository
+    {
+        public IEnumerable<Product> GetAllProducts();
+    }
+}
